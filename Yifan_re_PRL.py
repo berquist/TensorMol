2a488cc82288f291cb14b9eddcc880c8ba5bd77d
Your Name <you@example.com>
2017-02-27 16:36:40 -0500

    Save yifan file

--- Yifan_re_PRL.py
+++ Yifan_re_PRL.py
@@ -1,14 +1,27 @@
-''' YIfan's repeating code of the paper P.R.L 108,058301(2012) 'Fast and Accurate Modeling of Molecular Atomization Energies with Machine Learning'
+''' YIfan's repeating code of the paper P.R.L 108,058301(2012)
+ 'Fast and Accurate Modeling of Molecular Atomization Energies
+ with Machine Learning'
 '''
 from TensorMol import *
 
 b=MSet("gdb9")
-b.Load()
-
-print b.mols[-1]
+b.ReadXYZ("gdb9")
 # Load GDB9
+print(b.mols[0].coords)
+
+
+TreatedAtoms = b.AtomTypes()
+d=Digester(TreatedAtoms, name_="Coulomb", OType_="Energy")
+
 # Make a coulomb MolDigester which works over the whole molecule.
+
+tset=TensorData(b,d)
+
+tset.BuildTrainMolwise("gdb9_NEQ",TreatedAtoms)
 # Make a Tensordata and use it to buildtraining data
+'''
+#manager=TFManager("gdb9_NEQ_"+dig_)
 # Make a TFManager to produce a KRR instance.
 # Train.
 # Test.
+'''
